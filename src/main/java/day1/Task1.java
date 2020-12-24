4183c4561fb1e264f1a8f7f2c11f5c50f74b0b71
Mikhail Loginov <paytonklon@gmail.com>
2020-12-24 18:34:43 +0300

    Add Task1 solution

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,13 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int counter = 0;
+        while (true) {
+            System.out.print("JAVA ");
+            counter++;
+            if (counter == 10) {
+                break;
+            }
+        }
     }
 }
