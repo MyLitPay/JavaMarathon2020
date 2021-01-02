bc9b22c81bc072a46b3f69f27b23d37d585316bd
Mikhail Loginov <paytonklon@gmail.com>
2021-01-02 17:36:26 +0300

    Add Day8 solution

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,24 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long time0 = System.currentTimeMillis();
 
+        String str = "";
+        for (int i = 0; i <= 20000; i++) {
+            str += i + " ";
+        }
+        long time1 = System.currentTimeMillis() - time0;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+
+        long time2 = System.currentTimeMillis() - time0 - time1;
+
+        System.out.println(str);
+        System.out.println(sb.toString());
+        System.out.println("Concatenation: " + time1 + " mls");
+        System.out.println("StringBuilder: " + time2 + " mls");
     }
 }
