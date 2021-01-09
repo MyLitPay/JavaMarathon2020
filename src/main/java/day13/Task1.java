2720370819c12edbb7fa47a9e7b250aba93b399c
Mikhail Loginov <paytonklon@gmail.com>
2021-01-09 15:28:56 +0300

    Add Day13 solution

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,23 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User mike = new User("Mike");
+        User tom = new User("Tom");
+        User fred = new User("Fred");
 
+        mike.sendMessage(tom, "Hi");
+        mike.sendMessage(tom, "What's up");
+        tom.sendMessage(mike, "Hello, Mike");
+        tom.sendMessage(mike, "I'm fine");
+        tom.sendMessage(mike, "Where is my car, dude?");
+        fred.sendMessage(mike, "Aloha");
+        fred.sendMessage(mike, "That was a good whiskey");
+        fred.sendMessage(mike, "Thank you for advice");
+        mike.sendMessage(fred, "Aloha, my friend");
+        mike.sendMessage(fred, "I'm glad to hear this");
+        mike.sendMessage(fred, "Have you any plans on weekend?");
+        fred.sendMessage(mike, "Absolutely not");
+
+        MessageDatabase.showDialog(mike, fred);
     }
 }
