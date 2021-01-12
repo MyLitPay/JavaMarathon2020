477441ce953e5ad43c99a129d4d11406bbb51735
Mikhail Loginov <paytonklon@gmail.com>
2021-01-12 12:55:27 +0300

    Add Day15 solution

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,25 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/main/resources/shoes.csv");
+
+        try (Scanner scanner = new Scanner(file);
+        PrintWriter pw = new PrintWriter("src/main/resources/missing_shoes.txt")) {
+
+            while (scanner.hasNextLine()) {
+                String s = scanner.nextLine();
+                String[] arr = s.split(";");
+
+                if (arr[2].equals("0")) {
+                    pw.println(s.replaceAll(";", ", "));
+                }
+            }
+        }
     }
 }
