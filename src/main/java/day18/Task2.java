423ca31439b8c3669e4782870d8ff475c24664bd
Mikhail Loginov <paytonklon@gmail.com>
2021-01-13 17:53:11 +0300

    Add Day18 solution

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,17 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(count7(-1123780));
+    }
 
+    public static int count7(int num) {
+        int count = 0;
+        String strNum = String.valueOf(num);
+        if (strNum.contains("7") && !strNum.contains("-")) {
+            count++;
+            int newNum = Integer.parseInt(strNum.replaceFirst("7", ""));
+            count += count7(newNum);
+        }
+        return count;
     }
 }
--- src/main/java/day18/Task3.java
+++ src/main/java/day18/Task3.java
@@ -2,6 +2,68 @@ package day18;
 
 public class Task3 {
     public static void main(String[] args) {
+        Node root = new Node(20);
+        addNode(14, root);
+        addNode(23, root);
+        addNode(11, root);
+        addNode(16, root);
+        addNode(22, root);
+        addNode(27, root);
+        addNode(5, root);
+        addNode(15, root);
+        addNode(18, root);
+        addNode(24, root);
+        addNode(150, root);
+        addNode(8, root);
 
+        dfs(root);
     }
+
+    public static void addNode(int value, Node root) {
+        Node node = new Node(value);
+        if (value >= root.getValue()) {
+
+            if (root.getRightChild() == null) {
+                root.setRightChild(node);
+            } else {
+                addNode(value, root.getRightChild());
+            }
+        } else {
+
+            if (root.getLeftChild() == null) {
+                root.setLeftChild(node);
+            } else {
+                addNode(value, root.getLeftChild());
+            }
+        }
+    }
+
+    public static void dfs(Node root) {
+        Node first = root.getLeftChild();
+        if (first != null) {
+            dfs(first);
+
+        } else {
+            System.out.println(root.getValue());
+
+            if (root.getRightChild() != null) {
+                Node next = root.getRightChild();
+                dfs(next);
+            }
+        }
+
+        Node last = root.getRightChild();
+        if (last != null) {
+            dfs(last);
+
+        } else {
+            System.out.println(root.getValue());
+
+            if (root.getRightChild() != null) {
+                Node next = root.getRightChild();
+                dfs(next);
+            }
+        }
+    }
+
 }
